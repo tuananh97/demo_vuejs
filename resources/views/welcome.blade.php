0ec3eaa6cd71d155be8f0e5d97a75f6be69c84e0
tuananh97 <trananh9a@gmail.com>
2019-08-20 14:00:16 +0700

    Sample component Vuejs

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>Laravel</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+        </script>
+    </head>
+    <body>
+        <div id="app">
+            <my-component></my-component>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
